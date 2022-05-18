7525fdd6c597d17d77d647f67b28c55de1d8d027
Ansh Goyal <87718788+aghogwarts@users.noreply.github.com>
2022-05-18 16:40:26 +0530

    Add files via upload

--- ext/help.py
+++ ext/help.py
@@ -1,1 +1,15 @@
+from disnake import CommandInteraction
+from disnake.ext.commands import Bot, Cog, slash_command
 
+
+class Help(Cog):
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+
+    @slash_command(name="help", description="A simple slash-help command.")
+    async def help(self, inter: CommandInteraction) -> None:
+        await inter.send(f"Pong! {self.bot.latency * 1000:.2f}ms")
+
+
+def setup(bot: Bot) -> None:
+    bot.add_cog(Help(bot))
